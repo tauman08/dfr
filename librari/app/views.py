f99e385451ff3c5c691078e34aec2667e90775c6
a.timofeev <tauman08@gmail.com>
2022-12-22 21:55:36 +0300

    lesson_1

---
+++ librari/app/views.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Author
+from .serializers import AuthorModelSerializer
+
+
+class AuthorModelViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
