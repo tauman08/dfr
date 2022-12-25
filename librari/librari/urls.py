3a7604dc020adba443c8581621c4ca05fc20b24b
a.timofeev <tauman08@gmail.com>
2022-12-25 15:36:34 +0300

    lesson-2

--- librari/librari/urls.py
+++ librari/librari/urls.py
@@ -19,9 +19,10 @@ from rest_framework.routers import DefaultRouter
 from app.views import AuthorModelViewSet
 
 router = DefaultRouter()
-router.register('authors',AuthorModelViewSet)
+router.register('authors', AuthorModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls))
+    path('api/', include(router.urls)),
+
 ]
