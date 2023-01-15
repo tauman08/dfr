3dd0c8aadbf56ba9be331eb057c3d32fbaba80a0
Aleksandr Timofeev <41984641+tauman08@users.noreply.github.com>
2023-01-15 15:55:32 +0300

    Delete views.py

--- librari/app/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Author
-from .serializers import AuthorModelSerializer
-
-
-class AuthorModelViewSet(ModelViewSet):
-    queryset = Author.objects.all()
-    serializer_class = AuthorModelSerializer
