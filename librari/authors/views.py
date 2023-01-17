15496bd9eb83e23b557b93239254d258a4630c39
a.timofeev <tauman08@gmail.com>
2023-01-17 22:06:14 +0300

    lesson_4 & HW_4

--- librari/authors/views.py
+++ librari/authors/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from .models import Author, Book, Article, Biography
 from .serializers import AuthorModelSerializer, BookModelSerializer, ArticleModelSerializer, BiographyModelSerializer
+# from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework.generics import CreateAPIView, ListCreateAPIView
+from rest_framework.decorators import action
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class AuthorPaginator(LimitOffsetPagination):
+    default_limit = 10
 
 
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+    # ручками через переопределение родительского метода:
+    # def get_queryset(self):
+    #     # через заголовки
+    #     # param = self.request.headers.get('Filter')
+    #     # через параметры
+    #     param = self.request.query_params.get('name','')
+    #     if params:
+    #         # через заголовки
+    #         # return Author.objects.filter(first_name__contains=param)
+    #         # через параметры
+    #         return Author.objects.filter(first_name__contains=param[0])
+    #     return super().get_queryset()
+    # /////////////////////////////////////////////////////////////////
+    # через django_filters:
+    filterset_fields = ['first_name', 'last_name', 'birthday_year']
+    pagination_class = AuthorPaginator
 
 
 class BookModelViewSet(ModelViewSet):
@@ -22,3 +48,53 @@ class ArticleModelViewSet(ModelViewSet):
 class BiographyModelViewSet(ModelViewSet):
     queryset = Biography.objects.all()
     serializer_class = BiographyModelSerializer
+
+
+# class MyAPIView(APIView):
+
+#     def get(self, request):
+#         return Response({'data': 'GET SUCCESS'})
+
+#     def post(self, request):
+#         return Response({'data': 'POST SUCCESS'})
+
+# class MyAPIView(CreateAPIView):
+#     # создание объекта post
+#     renderer_classes = [JSONRenderer]
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorModelSerializer
+
+# class MyAPIView(ListCreateAPIView):
+#     # чтение данных объекта get
+#     renderer_classes = [JSONRenderer]
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorModelSerializer
+
+# class MyAPIView(CreateAPIView, ListCreateAPIView):
+#     # методы get и post
+
+#     # указываем конкретный рендер который хотим использовать, иначе будет использован
+#     # рендер по умолчанию
+#     # renderer_classes = [JSONRenderer]
+
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorModelSerializer
+
+class MyAPIView(ViewSet):
+
+    # create - post,
+    # retrieve - получение одного объекта
+    # update - update,
+    # destroy - delete,
+    # list- get(получение всех объектов)
+    # абревиатура - CRUD (create,retrieve,update, destroy)
+    # принятые в django обозначения методов mixins.py
+    def list(self, request):
+        queryset = Author.objects.all()
+        serializer = AuthorModelSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # реализуем свой произвольный метод. url создается автоматом - /my/babayka
+    @action(detail=False, methods=['get'])
+    def babayka(self, request):
+        return Response({'data': 'RATATA'})
--- librari/librari/urls.py
+++ librari/librari/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authors.views import AuthorModelViewSet, BookModelViewSet, ArticleModelViewSet, BiographyModelViewSet
+from authors.views import MyAPIView
+from todo_app.views import ProjectModelViewSet, ToDoModelViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
@@ -24,9 +26,17 @@ router.register('books', BookModelViewSet)
 router.register('article', ArticleModelViewSet)
 router.register('authors', AuthorModelViewSet)
 router.register('biography', BiographyModelViewSet)
+# вместо path('myapi/', MyAPIView.as_view({'get': 'list'})),
+# можно записать через роутер
+router.register('my', MyAPIView, basename='my')
+
+router.register('project', ProjectModelViewSet)
+router.register('todo', ToDoModelViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    # path('myapi/', MyAPIView.as_view({'get': 'list'})),
 ]
