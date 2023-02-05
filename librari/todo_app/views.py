bdd37b5056914815309b86307887a9214b612822
a.timofeev <tauman08@gmail.com>
2023-02-05 21:02:30 +0300

    lesson_7

--- librari/todo_app/views.py
+++ librari/todo_app/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
 from .models import Project, ToDo
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class ProjectPaginator(LimitOffsetPagination):
